63e9b72b4929bd6de436496955accc088e2d80b4
CodewithDeep23 <deepankarsinghvm@gmail.com>
2024-12-11 15:52:03 +0530

    Add necessary files for basicvite project

---
+++ basicvite/src/App.jsx
@@ -0,0 +1,16 @@
+import Practice from './new'
+
+function App() {
+  return (
+    // <h1>Hello Vite react</h1>
+    // <Practice/>
+    <>
+    <Practice/>
+    <h2>hello world</h2>
+    </>
+  )
+}
+
+export default App
+
+// Note: You only return one tag: Use <> </>
---
+++ basicvite/src/new.jsx
@@ -0,0 +1,7 @@
+function Practice(){
+    return(
+        <h1>Hello, this is for practice!</h1>
+    )
+}
+
+export default Practice
