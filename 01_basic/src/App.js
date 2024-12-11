61843f711cc4158018316a97a8002dfa21df9deb
CodewithDeep23 <deepankarsinghvm@gmail.com>
2024-12-11 14:18:48 +0530

    Add necessary files for 01_basic project

---
+++ 01_basic/src/App.js
@@ -0,0 +1,12 @@
+import Practice from './new'
+
+function App() {
+  return (
+    <>
+    <Practice/>
+    <h1>Hello world</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01_basic/src/new.js
@@ -0,0 +1,7 @@
+function Practice(){
+    return(
+        <h1>Hello, this is a basic</h1>
+    )
+}
+
+export default Practice
