952e043aa7d65ffa99fd74f841dfacd500c5f568
CodewithDeep23 <deepankarsinghvm@gmail.com>
2024-12-12 10:55:57 +0530

    react element

--- basicvite/src/App.jsx
+++ basicvite/src/App.jsx
@@ -1,14 +1,18 @@
 import Practice from './new'
 
 function App() {
+
+  const username = "Dsk23lite"
   return (
     // <h1>Hello Vite react</h1>
     // <Practice/>
     <>
     <Practice/>
-    <h2>hello world</h2>
+    <h2>hello world {username}</h2>
     </>
   )
+
+  // {username} in h2: is a evaluted expression means: This is a final outcome of Js
 }
 
 export default App
--- basicvite/src/main.jsx
+++ basicvite/src/main.jsx
@@ -1,7 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <App />
+// We also create a function in this compenent
+function NewReact(){
+    return(
+        <div>
+            <h1>
+                Hello, this is a NewReact
+            </h1>
+        </div>
+    )
+}
+
+// Ok, What happened when we add custom react in this compenent
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     childNode: 'Click me'
+// }
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'Visit google'
+)
+
+// BTS: there are many algo working
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    // reactElement
+    <>
+    <App/>
+    <NewReact/>
+    </>
+
+    // reactElement // some error, because this is object not a function and also there is no chance that in my object and react have same prop
 )
